e85e8f9ee022d7cf06190fdd847e5ab9b54a0f83
josephe44 <josephe442@gmail.com>
2024-01-09 14:42:59 +0100

    change log to fmt

--- initializers/env.go
+++ initializers/env.go
@@ -1,7 +1,7 @@
 package initializers
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -11,7 +11,8 @@ func LoadEnvVariables() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		// log.Fatal("Error loading .env file")
+		fmt.Print("Error loading .env file")
 	}
 	// Load environment variables if needed
 	os.Getenv("RENDER_API_KEY")
